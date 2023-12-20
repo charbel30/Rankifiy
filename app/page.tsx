cc01400ab3deabb8a544eb68a21fcf7597ead257
charbel30 <charbeltannous30@gmail.com>
2023-12-20 12:46:59 +0200

    Remove unused code and import

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 
-
 import { HeroHeader } from '@/components/HeroHeader/HeroHeader';
-import PopularTierLists from '@/components/HeroHeader/PopularTierLists';
-import { mockTierLists } from '@/public/mockData';
-
 type TierList = {
   title: string;
   description: string;
@@ -14,15 +10,7 @@ type TierList = {
 type HomePageProps = {
   lists: TierList[];
 };
-export async function getServerSideProps() {
-  const lists = mockTierLists;
 
-  return {
-    props: {
-      lists,
-    },
-  }
-}
 // export async function getServerSideProps() {
 //   const res = await fetch('/api/tier-lists?sort=popularity&limit=3') // Replace with your actual API endpoint
 //   const lists = await res.json()
@@ -37,7 +25,6 @@ export default function HomePage({lists}: HomePageProps) {
   return (
     <>
       <HeroHeader />
-      <PopularTierLists lists={lists} />
     </>
   );
 }
