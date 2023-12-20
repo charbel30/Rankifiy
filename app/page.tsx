956252dd556d9faf56f72c06774c703d30ecb14b
charbel30 <charbeltannous30@gmail.com>
2023-12-20 12:50:37 +0200

    Remove unused code and add newline at end of file

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,10 @@
-
 import { HeroHeader } from '@/components/HeroHeader/HeroHeader';
-type TierList = {
-  title: string;
-  description: string;
-  // Add more properties as needed
-};
 
-// Define the type for the HomePage props
-type HomePageProps = {
-  lists: TierList[];
-};
-
-// export async function getServerSideProps() {
-//   const res = await fetch('/api/tier-lists?sort=popularity&limit=3') // Replace with your actual API endpoint
-//   const lists = await res.json()
-
-//   return {
-//     props: {
-//       lists,
-//     },
-//   }
-// }
-export default function HomePage({lists}: HomePageProps) {
+export default function HomePage() {
   return (
     <>
       <HeroHeader />
+     
     </>
   );
 }
